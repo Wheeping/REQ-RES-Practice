938c2cf177d18875aa79857b93245ce41407f549
John Moore <jdmoore697@gmail.com>
2019-08-29 00:30:48 -0500

    added more functions to script

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+/*--------GLOBAL VARIABLES--------*/
+//Declaring the website I'm using for this program.
 const reqresAPI = "https://reqres.in/api/users";
+
+//Global variables to hold the total number of pages and the length of the arrays.
 let totalPages;
 let arrayLength;
 
+//Global variables to hold the current row information.
+let row;
+let cellId;
+let cellEmail;
+let cellFirstName;
+let cellLastName;
+let cellAvatar;
+/*--------END GLOBAL VARIABLES--------*/
+
+/*-------------FUNCTIONS-------------*/
 //Finds the total amount of pages. Have to use seperate function to assign it.
 function findNumberOfPages(website){
 	$.getJSON(website, function(pulledJSON){
@@ -9,54 +23,55 @@ function findNumberOfPages(website){
 	});
 }
 
-//Stupid function. Just thinking it out.
-/*function loopThroughPages(website){
-	findNumberOfPages(website);
-	let pageNumber = 1;
-	while (pageNumber <= totalPages){
-		pageNumber++;
-	}
-}*/
-
 //Finds the length of the array
-function findArrayLength(website, pageNumber){
+function findDataArrayLength(website, pageNumber){
 		$.getJSON(website + '?page=' + pageNumber, function(pulledJSON){
 			arrayLength = pulledJSON.data.length;
 	});
 }
 
-/*
-let pageNumber = 1;
-	while (pageNumber <= totalPages){
-		$.getJSON(reqresAPI + pageNumber, function(passthrough){
-			let arrayLength = passthrough.data.length;
-			let arrayNumber = 0;
-			while (arrayNumber < arrayLength)
-			{
-				let gotId = passthrough.data[arrayNumber].id;
-				let gotEmail = passthrough.data[arrayNumber].email;
-				let gotFirstName = passthrough.data[arrayNumber].first_name;
-				let gotLastName = passthrough.data[arrayNumber].last_name;
-				let gotAvatar = passthrough.data[arrayNumber].avatar;
-				var img = document.createElement('img');
-    			img.src = gotAvatar;
+//Adds new row to table
+function addRow(table){
+	row = table.insertRow(1);
+	cellId = row.insertCell(0);
+	cellEmail = row.insertCell(1);
+	cellFirstName = row.insertCell(2);
+	cellLastName = row.insertCell(3);
+	cellAvatar = row.insertCell(4);
+}
 
-				var table = document.getElementById("userTable");
-				var row = table.insertRow(1);
-				var cellId = row.insertCell(0);
-				var cellEmail = row.insertCell(1);
-				var cellFirstName = row.insertCell(2);
-				var cellLastName = row.insertCell(3);
-				var cellAvatar = row.insertCell(4);
-				cellId.innerHTML = gotId;
-				cellEmail.innerHTML = gotEmail;
-				cellFirstName.innerHTML = gotFirstName;
-				cellLastName.innerHTML = gotLastName;
-				cellAvatar.appendChild(img);
+//assigns the pulled information to the row
+function assignUserDataToRow(website, pageNumber, arrayNumber){
+	$.getJSON(website + '?page=' + pageNumber, function(pulledJSON){
+		let pulledId = pulledJSON.data[arrayNumber].id;
+		let pulledEmail = pulledJSON.data[arrayNumber].email;
+		let pulledFirstName = pulledJSON.data[arrayNumber].first_name;
+		let pulledLastName = pulledJSON.data[arrayNumber].last_name;
+		let pulledAvatar = pulledJSON.data[arrayNumber].avatar;
+		let image = document.createElement('img');
+		image.src = pulledAvatar;
+		cellId.innerHTML = pulledId;
+		cellEmail.innerHTML = pulledEmail;
+		cellFirstName.innerHTML = pulledFirstName;
+		cellLastName.innerHTML = pulledLastName;
+		cellAvatar.appendChild(image);
+	});
+}
 
-				arrayNumber++;
-			};
-		});
-		pageNumber++;
-	};
-	*/
+//Display all users onto table
+function displayAllUsers(website, table){
+	findNumberOfPages(website);
+	let currentPage = 1;
+	while (currentPage <= totalPages){
+		console.log("ping")
+		findDataArrayLength(website, currentPage);
+		addRow(table);
+		let arrayNumber = 1;
+		while (arrayNumber <= arrayLength){
+			assignUserDataToRow(website, currentPage, arrayNumber)
+			arrayNumber++;
+		}
+		currentPage++;
+	}
+}
+/*------------END FUNCTIONS------------*/
